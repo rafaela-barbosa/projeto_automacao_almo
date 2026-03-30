e2fa56141f4a327fd50bc2e635be1a6ab0fd9cf2
Rafaela Barbosa <rafaelabarbos415@gmail.com>
2026-03-30 02:04:49 -0300

    docs: adiciona guia de configuração .env

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&dmf^(5(g3odff5v$9ma54(lf&)q1&*)=q8j^ck=s$+*=qi=ik'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
